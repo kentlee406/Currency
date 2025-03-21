78a428a6ba2e2c5682e3f6b2194c9394cfcc5642
kentlee406 <157895655+kentlee406@users.noreply.github.com>
2025-03-21 21:07:54 +0800

    Android APP Intro-HW1

---
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -0,0 +1,31 @@
+package com.home.currency;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        EditText ntd = findViewById(R.id.ntd);
+//        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+//        TextView us = findViewById(R.id.us);
+//        TextView jp = findViewById(R.id.jp);
+//        Button btn = findViewById(R.id.button2);
+
+
+
+    }
+}
